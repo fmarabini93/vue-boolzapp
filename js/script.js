9ecb4bcb049b81cc742634854836a15cdd503578
fmarabini93 <fmarabini@icloud.com>
2021-05-21 09:31:51 +0200

    last-message-and-date-seen-on-left-side

--- js/script.js
+++ js/script.js
@@ -4,23 +4,23 @@ const app = new Vue(
         data: {
             contacts: [
                 {
-                    name: 'Michele',
+                    name: 'Michael',
                     avatar: '_1',
                     visible: true,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
-                            text: 'Hai portato a spasso il cane?',
+                            text: 'Have you taken dog for a walk?',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
-                            text: 'Ricordati di dargli da mangiare',
+                            text: 'Remember feeding him',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 16:15:22',
-                            text: 'Tutto fatto!',
+                            text: 'All done!',
                             status: 'received'
                         }
                     ],
@@ -32,17 +32,17 @@ const app = new Vue(
                     messages: [
                         {
                             date: '20/03/2020 16:30:00',
-                            text: 'Ciao come stai?',
+                            text: 'Hi, how are you?',
                             status: 'sent'
                         },
                         {
                             date: '20/03/2020 16:30:55',
-                            text: 'Bene grazie! Stasera ci vediamo?',
+                            text: 'Fine thanks! Shall we meet tonight?',
                             status: 'received'
                         },
                         {
                             date: '20/03/2020 16:35:00',
-                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            text: 'I would really like to, but sadly I have to go to grocery',
                             status: 'sent'
                         }
                     ],
@@ -54,17 +54,17 @@ const app = new Vue(
                     messages: [
                         {
                             date: '28/03/2020 10:10:40',
-                            text: 'La Marianna va in campagna',
+                            text: 'Marianne goes to countryside',
                             status: 'received'
                         },
                         {
                             date: '28/03/2020 10:20:10',
-                            text: 'Sicuro di non aver sbagliato chat?',
+                            text: 'Are you sure this is the right chat?',
                             status: 'sent'
                         },
                         {
                             date: '28/03/2020 16:15:22',
-                            text: 'Ah scusa!',
+                            text: 'Ah, sorry!',
                             status: 'received'
                         }
                     ],
@@ -76,12 +76,12 @@ const app = new Vue(
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
-                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            text: 'Do you know that a new pizzeria has opened?',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
-                            text: 'Si, ma preferirei andare al cinema',
+                            text: 'Yes, but I prefer going to the cinema',
                             status: 'received'
                         }
                     ],
